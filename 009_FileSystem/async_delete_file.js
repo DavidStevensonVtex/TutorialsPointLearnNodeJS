3c5d38a66ce4c0ee53f0535005125f529ea3ce29
David Stevenson <drstevenson1958@yahoo.com>
2023-07-05 19:28:28 -0400

    Asynchronous Delete File

---
+++ 009_FileSystem/async_delete_file.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+console.log("Going to delete an existing file");
+fs.unlink('input.txt', function(err) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("File deleted successfully!");
+});
+
+// Going to delete an existing file
+// File deleted successfully!
